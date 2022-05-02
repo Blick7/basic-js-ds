e5966c3b2936b27787e44cdb136dd5a185391c3b
Blick <45530302+Blick7@users.noreply.github.com>
2022-05-02 22:12:13 +0300

    feat: complete 3 tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  construct() {
+    this.first = null;
+    this.last = null;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.first;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let listNode = new ListNode(value);
+    if (this.first == null) {
+      this.first = listNode;
+      this.last = this.first;
+    }
+    this.last.next = listNode;
+    this.last = this.last.next;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let topEl = this.first.value;
+    this.first = this.first.next;
+    return topEl;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
